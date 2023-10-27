e2fda88cb63dcc3b9800ed766310f7a7a86fe485
Tiago Santos <tfsantosbr@gmail.com>
2023-10-27 17:36:24 -0300

    fix: sonnar warnings

--- src/SampleApp.Domain/Users/Handlers/UserCommandsHandler.cs
+++ src/SampleApp.Domain/Users/Handlers/UserCommandsHandler.cs
@@ -82,7 +82,7 @@ namespace SampleApp.Domain.Users.Handlers
             var user = _userRepository.GetById(request.UserId);
 
             if (user is null)
-                throw new ArgumentNullException(nameof(user));
+                throw new NullReferenceException(nameof(user));
 
             user.UpdateDetails(
                 completeName: new CompleteName(request.FirstName, request.LastName)
@@ -119,7 +119,7 @@ namespace SampleApp.Domain.Users.Handlers
             var user = _userRepository.GetById(request.UserId);
 
             if (user is null)
-                throw new ArgumentNullException(nameof(user));
+                throw new NullReferenceException(nameof(user));
 
             user.UpdatePassword(new Password(request.Password));
 
@@ -143,7 +143,7 @@ namespace SampleApp.Domain.Users.Handlers
             var user = _userRepository.GetById(request.UserId);
 
             if (user is null)
-                throw new ArgumentNullException(nameof(user));
+                throw new NullReferenceException(nameof(user));
 
             _userRepository.Remove(user);
 
